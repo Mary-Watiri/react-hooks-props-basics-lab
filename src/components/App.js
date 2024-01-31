1ab615315bc52f4b5934c48b23d24ecdf24f2514
Mary Watiri <watirimary30@gmail.com>
2024-01-31 22:11:54 +0300

    I still have one test failing

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home username= {user.name} city={user.city} color={user.color} />
+      <Home username = {user.name} city = {user.city} color = {user.color} />
       <About bio={user.bio} links={user.links} />
     </div>
   );
